f5b6770e5322eceba8245471a26551c34c00588b
Diego Sepúlveda <diegosepu.soto@gmail.com>
2021-09-10 18:41:09 -0300

    refactorizando código

--- app/src/main/java/com/example/tarea1/Activity2.java
+++ app/src/main/java/com/example/tarea1/Activity2.java
@@ -12,21 +12,22 @@ import android.widget.Toast;
 public class Activity2 extends AppCompatActivity {
 
     Button boton2;
+    Intent volverIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i( "Activity2", "Abriendo Activity 2");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
 
-        Log.i( "Activity2", "onCreate");
-
         boton2 = findViewById(R.id.boton2);
+        volverIntent = new Intent(Activity2.this, MainActivity.class);
 
         boton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                Intent intent = new Intent(Activity2.this, MainActivity.class);
-                startActivity(intent);
-
+                startActivity(volverIntent);
                 finish();
             }
         });
@@ -35,6 +36,6 @@ public class Activity2 extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
 
-        Log.i("Activity2", "onStop: ");
+        Log.i("Activity2", "Cerrando Activity2");
     }
 }
--- app/src/main/java/com/example/tarea1/MainActivity.java
+++ app/src/main/java/com/example/tarea1/MainActivity.java
@@ -12,27 +12,26 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     Button boton;
+    Intent irActividad2Intent;
+    Toast irActividad2Toast;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i( "MainActivity", "Abriendo MainActivity");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
-        Log.i( "MainActivity", "onCreate");
-
         boton = findViewById(R.id.boton1);
+        irActividad2Intent = new Intent(MainActivity.this, Activity2.class);
+        irActividad2Toast = Toast.makeText(MainActivity.this, "Ir a la actividad 2", Toast.LENGTH_LONG);
 
         boton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                Intent intent = new Intent(MainActivity.this, Activity2.class);
-                startActivity(intent);
-                Toast.makeText(MainActivity.this, "Ir a la actividad 2", Toast.LENGTH_LONG).show();
-
+                startActivity(irActividad2Intent);
+                irActividad2Toast.show();
             }
         });
     }
-
-
 }
